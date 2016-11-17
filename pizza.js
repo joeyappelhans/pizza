f02d58e8e5fb8d2b2d9fc93d1a6309026d1c61b8
joeyappelhans <9jappelhans@stpatrick.org>
2016-11-17 13:56:20 -0600

    instance functions almost complete

---
+++ pizza.js
@@ -0,0 +1,45 @@
+class pizza{
+  //type constructor below
+constructor(s,mt,vt){
+  this.size=s;
+  this.meatToppings=mt;
+  this.veggieToppings=vt;
+  var size=["S","M","L","XL"];
+  var meatToppings=[];
+  var veggieToppings=[];
+}
+  //type instance functions below
+sizeCost(){
+  if(this.size[0]){
+    price=7.99;
+    return price
+  }
+  if(this.size[1]){
+    price=9.99;
+    return price
+  }
+  if(this.size[2]){
+    price=12.99;
+    return price
+  }
+  if(this.size[3]){
+    price=15.99;
+    return price
+  }
+}
+toppingCost(top){
+  if(top=meatToppings[]){
+    meat=meatToppings*.99;
+    return meat
+  }
+  if(top=veggieToppings[]){
+    veggie=veggieToppings[]*.5;
+    return veggie
+  }
+}
+
+
+  //type class functions below
+
+
+}
