9dc9611da7f651214c722fe620e37f419f4d90f4
joeyappelhans <9jappelhans@stpatrick.org>
2016-11-18 13:48:40 -0600

    finished

--- pizza.js
+++ pizza.js
@@ -1,45 +1,49 @@
 class pizza{
   //type constructor below
-constructor(s,mt,vt){
-  this.size=s;
-  this.meatToppings=mt;
-  this.veggieToppings=vt;
-  var size=["S","M","L","XL"];
-  var meatToppings=[];
-  var veggieToppings=[];
-}
+  constructor(s,mt,vt){
+    this.size=s;
+    this.meatToppings=mt;
+    this.veggieToppings=vt;
+    var size=["S","M","L","XL"];
+    var meatToppings=[];
+    var veggieToppings=[];
+  }
   //type instance functions below
-sizeCost(){
-  if(this.size[0]){
-    price=7.99;
-    return price
+  sizeCost(){
+    if(this.size[0]){
+      price=7.99;
+      return price
+    }
+    if(this.size[1]){
+      price=9.99;
+      return price
+    }
+    if(this.size[2]){
+      price=12.99;
+      return price
+    }
+    if(this.size[3]){
+      price=15.99;
+      return price
+    }
   }
-  if(this.size[1]){
-    price=9.99;
-    return price
+  toppingCost(top){
+    if(top=meatToppings[]){
+      meat=meatToppings*0.99;
+      return meat
+    }
+    if(top=veggieToppings[]){
+      veggie=veggieToppings[]*0.5;
+      return veggie
+    }
   }
-  if(this.size[2]){
-    price=12.99;
-    return price
+  price(){
+    totalcost=price+meat+veggie;
+    return totalcost
   }
-  if(this.size[3]){
-    price=15.99;
-    return price
-  }
-}
-toppingCost(top){
-  if(top=meatToppings[]){
-    meat=meatToppings*.99;
-    return meat
-  }
-  if(top=veggieToppings[]){
-    veggie=veggieToppings[]*.5;
-    return veggie
-  }
-}
-
-
   //type class functions below
-
-
+  static promotionalDeal(pizza,percent){
+    totalcost=pizza*percent/100;
+    return totalcost
+  }
 }
